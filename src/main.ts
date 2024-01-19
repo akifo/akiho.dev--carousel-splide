304752cf3d122e11a061e75dff8825f724d9c25b
akifo <akihoho@gmail.com>
2024-01-19 12:40:31 +0900

    Add image carousel using Vue Splide

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import VueSplide from "@splidejs/vue-splide";
+// デフォルトのテーマ
+import "@splidejs/vue-splide/css";
+// または、ほかのテーマ
+// import "@splidejs/vue-splide/css/skyblue";
+// import "@splidejs/vue-splide/css/sea-green";
+// あるいは、コアスタイルのみ
+// import "@splidejs/vue-splide/css/core";
 
-createApp(App).mount("#app");
+createApp(App).use(VueSplide).mount("#app");
